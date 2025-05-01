7df9aeeb70e682e939d2bbc25e8fdf34438585c8
Guillermo Esquivel <esquivelguillerm@gmail.com>
2025-05-01 15:59:35 -0600

    New file

---
+++ tests/twoNAddition.cpp
@@ -0,0 +1,14 @@
+// C++ program to add two number using
+// addition operator
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a = 11;
+    float b = "3";
+
+    // Adding the two numbers and printing
+    // their sum
+    cout << a + b;
+    return 0;
+}
